275e29b0b92012532725772286d12cc935de18d4
izbay <jacobmartinek@knights.ucf.edu>
2015-02-07 17:36:37 -0500

    Added "Subject" Agent
    It moves, rotates, and remembers where the heck it is! :D

---
+++ Assets/subjectController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class subjectController : MonoBehaviour {
+
+	public float turnSpeed;
+	public float moveSpeed;
+
+	private float curr_rot;
+	private float curr_x;
+	private float curr_y;
+
+	void Start () {
+		savePosition ();
+	}
+
+	void Update () {
+		// Handle if W and S are both held.
+		int dir_motion = 0;
+		if (Input.GetKey (KeyCode.W))
+			dir_motion++;
+		if (Input.GetKey (KeyCode.S))
+			dir_motion--;
+		
+		// Do motion.
+		if (dir_motion != 0)
+			transform.Translate (dir_motion * Vector3.right * moveSpeed * Time.deltaTime);
+
+		// Handle if A and D are both held.
+		int dir_rotation = 0;
+		if (Input.GetKey (KeyCode.A))
+			dir_rotation++;
+		if (Input.GetKey (KeyCode.D))
+			dir_rotation--;
+
+		// Do rotation.
+		if (dir_rotation != 0)
+			transform.Rotate(dir_rotation * Vector3.forward * turnSpeed * 10 * Time.deltaTime);
+
+		savePosition ();
+	}
+
+	void savePosition () {
+		curr_x = transform.position.x;
+		curr_y = transform.position.y;
+		curr_rot = transform.rotation.eulerAngles.z;
+
+		//print ("(" + curr_x + ", " + curr_y + ") @ " + curr_rot);
+	}
+}
