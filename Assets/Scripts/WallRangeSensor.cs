da3ad6a61918abcf61e8adb2b167e2394a436c8a
simolias <simolias@gmail.com>
2015-02-15 22:41:00 -0500

    Wall Sensor Raycast
    The implementation of the raycasts to dertect whether a wall has been
    detected, outputs the distance of the walls to the UI

--- Assets/Scripts/WallRangeSensor.cs
+++ Assets/Scripts/WallRangeSensor.cs
@@ -6,26 +6,24 @@ using System.Collections.Generic;
 public class WallRangeSensor : MonoBehaviour {
 	
 	public Text UItext;
-	
-	private List<Transform> adjacentWalls = new List<Transform> ();
-	private RaycastHit2D hit;
-	void OnTriggerEnter2D (Collider2D other)
-	{
-		if (other.CompareTag("wall"))
-			adjacentWalls.Add (other.transform);
+	public float range=1.0f,l=1.0f,f=1.0f,r=1.0f;
+	private List<float> adjacentWalls = new List<float> ();
+	public RaycastHit2D leftRayHit, rightRayHit, frontRayHit;
+
+	void Start(){
+		//adds in the 3 sensors to the list
+		adjacentWalls.Add (l);
+		adjacentWalls.Add (f);
+		adjacentWalls.Add (r);
+
 	}
-	
-	void OnTriggerExit2D (Collider2D other)
-	{
-		if (other.CompareTag("wall"))
-			adjacentWalls.Remove(other.transform);
-	}
-	
+
 	void Update ()
 	{
-		Transform leftRay = transform;
-		Transform rightRay = transform;
-
+		//detects if the rays hit an object that doesnt belong tot he layer Ignore Raycast
+		frontRayHit = Physics2D.Raycast (transform.position, (transform.right)*range,1<<LayerMask.NameToLayer("Ignore Raycast"));
+		leftRayHit = Physics2D.Raycast (transform.position, (transform.up+(transform.right/2))*range,1<<LayerMask.NameToLayer("Ignore Raycast"));
+		rightRayHit = Physics2D.Raycast (transform.position, (-transform.up+(transform.right/2))*range,1<<LayerMask.NameToLayer("Ignore Raycast"));
 
 		UpdateGUI();
 	}
@@ -34,18 +32,33 @@ public class WallRangeSensor : MonoBehaviour {
 	{
 		string output = "Detected Walls:\n";
 		
-		foreach (Transform t in adjacentWalls)
-		{
-			Vector3 distance = t.position - transform.position;
-			output = output + "(" + distance.magnitude.ToString("F1") + ")";
+		//if the front ray has hit something, display the output and update the f float, else display normal ray and set f to range
+		if (!frontRayHit) {
+			Debug.DrawRay (transform.position, (transform.right)*range,Color.red);
+			f=range;
+		} else {
+			Debug.DrawRay (transform.position, (transform.right)*range,Color.cyan);
+			output = output + "(" + frontRayHit.distance.ToString("F1") + ")";
+			f=frontRayHit.distance;
 		}
-		Debug.DrawRay (transform.position + (transform.forward * 4), transform.right - transform.up * 2, Color.red,0.0f,true);
-		Debug.DrawRay (transform.position + (transform.forward * 4), transform.right * 2, Color.blue,0.0f,true);
-		Debug.DrawRay (transform.position + (transform.forward * 4), transform.right + transform.up * 2, Color.green,0.0f,true);
-
-		//Debug.DrawRay (transform.position - (transform.forward * 4), - transform.right * 20, Color.yellow,20f,true);
-		
-		//Debug.DrawRay (transform.position - (transform.forward * 4), transform.right * 20, Color.yellow,20f,true);
+		//if the left ray has hit something, display the output and update the l float, else display normal ray and set l to range
+		if (!leftRayHit) {
+			Debug.DrawRay (transform.position, (transform.up+(transform.right/2))*range, Color.blue);
+			l=range;
+		} else {
+			Debug.DrawRay (transform.position, (transform.up+(transform.right/2))*range, Color.cyan);
+			output = output + "(" + leftRayHit.distance.ToString("F1") + ")";
+			l=leftRayHit.distance;
+		}
+		//if the right ray has hit something, display the output and update the r float, else display normal ray and set r to range
+		if (!rightRayHit) {
+			Debug.DrawRay (transform.position, (-transform.up+(transform.right/2))*range, Color.green);
+			r=range;
+		} else {
+			Debug.DrawRay (transform.position, (-transform.up+(transform.right/2))*range, Color.cyan);
+			output = output + "(" + rightRayHit.distance.ToString("F1") + ")";
+			r=rightRayHit.distance;
+		}
 
 		UItext.text = output;
 	}
